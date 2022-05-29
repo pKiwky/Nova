46ea939e36af9b7a55bfb03b739c31006f6b934f
pKiwky <Picuandreia2001@gmail.com>
2022-05-29 15:12:15 +0300

    Events base class

---
+++ Nova.Discord/src/Events/DmChannelDeleted.cs
@@ -0,0 +1,28 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+using Nova.Discord.Core;
+using Nova.Discord.Interfaces;
+using Nova.Discord.Managers;
+using Nova.Discord.Models;
+
+namespace Nova.Discord.Events {
+
+    public class DmChannelDeleted : IEvent<DmChannelDeleteEventArgs> {
+        public Task Run(DiscordClient client, DmChannelDeleteEventArgs args) {
+            if (NovaManager.Instance().Guilds.TryGetValue(args.Channel.Guild.Id, out NovaGuild novaGuild) == false) {
+                return Task.CompletedTask;
+            }
+
+            foreach (var module in BaseModule.Modules) {
+                if (novaGuild.GetSetting<bool>(module.ModuleName) == false) {
+                    continue;
+                }
+
+                module?.OnDmChannelDeleted(novaGuild, client, args);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+}
